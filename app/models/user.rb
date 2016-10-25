27775bd3e0be76b3fd4fdbf65dba9fc41e3f1aee
Jean Jacques MBRA <kouassi.mbra@gmail.com>
2016-10-25 10:41:44 +0200

    Addind secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
 class User < ActiveRecord::Base
-	has_one :profile, dependent: :destroy
-	has_many :todo_lists, dependent: :destroy
-	has_many :todo_items, through: :todo_lists, source: :todo_items
-	validates :username, presence: true
+  has_secure_password
+  has_one :profile, dependent: :destroy
+  has_many :todo_lists, dependent: :destroy
+  has_many :todo_items, through: :todo_lists, source: :todo_items
+  validates :username, presence: true
 
-	def get_completed_count
-		tab = []
-		User.first.todo_items.each do |item|
-			if item.completed == true
-				tab << item
-			end
-		end
-		tab.count
+  def get_completed_count
+    tab = []
+	User.first.todo_items.each do |item|
+	  if item.completed == true
+	    tab << item
+	  end
 	end
+	tab.count
+  end
 end
