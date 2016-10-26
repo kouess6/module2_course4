73f937ae59ebadf3593cb5a8c834a5261f355a9c
Jean Jacques MBRA <kouassi.mbra@gmail.com>
2016-10-26 09:37:26 +0200

    Modify routes.rb file to set todoitems as todolists nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
-  resources :todo_items
-  resources :todo_lists
-  resources :profiles
+  
+  root to: "todo_lists#index"
   resources :users
+  resources :profiles
+  resources :todo_lists do
+    resources :todo_items
+  end
+  
+  
+
+  
+
+  #get "/todo_lists" => "todo_lists#index"
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
