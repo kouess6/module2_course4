6c7d7ff70067860a27c89b7bc3eccc70180e6498
Jean Jacques MBRA <kouassi.mbra@gmail.com>
2016-11-02 20:19:07 +0100

    Question 10 OK without error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
   root to: "todo_lists#index"
   resources :todo_lists do
     resources :todo_items
